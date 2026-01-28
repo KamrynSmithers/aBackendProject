a9815c8ad4b05f71b8334dde9155434e3473aef0
KamrynSmithers <kamrynaliecesmithers@gmail.com>
2026-01-27 22:21:19 -0500

    Task and Project schema created and ready for importing

---
+++ models/Project.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+})
+
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
--- models/Task.js
+++ models/Task.js
@@ -1,3 +1,26 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    maxlength: [100, 'Task Title Cannot Exceed 100 Characters']
+  },
+  description: {
+    type: String,
+    maxlength: [3000, 'Description cannot exceed 3000 characters. Sorry!']
+  },
+  status: {
+    type: String,
+    enum: ["Why Haven't You Started On This?", "Started.. But Haven't Finished", "DONE!"],
+    default: "Why Haven't You Started On This?"
+  },
+  project: {
+    type: Schema.Types.ObjectId,
+    ref: 'Project',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Task', taskSchema);
